865093cb2728ea957d855f2707e72f2a3c3b3945
Alva <morox.alvarox@gmail.com>
2021-09-22 17:37:37 -0300

    mejore todo

--- eventsOfPage.js
+++ eventsOfPage.js
@@ -1,9 +1,32 @@
-let btnMenu = document.getElementById('submenuBtn');
-let submenu = btnMenu.firstElementChild.nextElementSibling;
-function menuToggler (elementToggled,strClassToAdd) {
-    elementToggled.firstElementChild.nextElementSibling.classList.toggle ('submenuClicked')
-  }
-btnMenu.addEventListener("click", function() {
-    menuToggler(btnMenu);
-})
+//Variables and Constants.
+const windowWidth = window.innerWidth
+|| document.documentElement.clientWidth
+|| document.body.clientWidth;
+/*  */
+const btnMenu = document.getElementById('submenuBtn');
+const submenu = btnMenu.firstElementChild.nextElementSibling;
+const responsiveMenu = document.querySelector('#responsiveMenu');
+//show and tells
+console.log(btnMenu);
+console.log(windowWidth);
+console.log(responsiveMenu);
+console.log(responsiveMenu.parentNode.nextElementSibling.firstElementChild);
+
+
+//Predefined Functions.
+function menuFirstChildToggler (elementToggled,strClassToAdd) {
+  elementToggled.firstElementChild.nextElementSibling.classList.toggle (strClassToAdd)};
+function menuCousinToggler (elementToggled,strClassToAdd) {
+  elementToggled.parentNode.nextElementSibling.firstElementChild.classList.toggle (strClassToAdd)}
+
+/*  */
+btnMenu.addEventListener("click", function(){
+  menuFirstChildToggler (btnMenu,"subMenuClicked")
+});
+responsiveMenu.addEventListener("click", function(){
+  menuCousinToggler (responsiveMenu,"responsiveMenuClicked")
+});
+  
+
+
 
